b593d94aac3f1e64355123d30cabbcd004291be0
procodrr <76770313+procodrr@users.noreply.github.com>
2022-11-06 10:04:38 +0530

    Add files via upload

---
+++ ep-04_javascript-variables/final-code/scirpt.js
@@ -0,0 +1,16 @@
+console.log(firstName)
+
+var firstName = 'Adarsh'
+let lastName = 'Singh'
+let age = 15
+let खुशहै = true
+firstName = 100
+var a
+
+const hoursInDay = 24
+
+
+let userIntro = 
+    'Hi, my name is ' 
+    + firstName + ' ' 
+    + lastName + '. I am ' + age + ' ' + 'years old.'
