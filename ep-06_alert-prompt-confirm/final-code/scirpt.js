561843ae5d5366c470fe11df08d27f540290d1cc
procodrr <76770313+procodrr@users.noreply.github.com>
2022-11-08 08:22:35 +0530

    Add files via upload

---
+++ ep-06_alert-prompt-confirm/final-code/scirpt.js
@@ -0,0 +1,4 @@
+// const alertResult = alert("Are you sure?")
+// const isConfirmed = confirm("Would you like to proceed?")
+const userInput = prompt("Please enter your name.........................")
+console.log(userInput)
