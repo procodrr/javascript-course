dec517654fba21694416d2248f45a157ab10b05c
procodrr <76770313+procodrr@users.noreply.github.com>
2022-11-27 22:48:37 +0530

    Add files via upload

---
+++ ep-24-compound-operators/final-code/script.js
@@ -0,0 +1,33 @@
+const username = 'Anurag'
+
+let num = 5
+
+// debugger
+// num = num + 5
+// num = num + 5
+// num = num + 5
+// num = num + 5
+
+// num += 5
+// num += 5
+// num += 5
+// num += 5
+
+// num = num * 2
+// num = num * 2
+// num = num * 2
+
+// num *= 2
+// num *= 2
+// num *= 2
+
+// const newNum = ++num
+// const newNum = --num
+
+// const newNum = num++
+// const newNum = num--
+
+// num = num + 1
+// num += 1
+
+num++
