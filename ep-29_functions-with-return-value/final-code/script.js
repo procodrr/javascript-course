9ffb99fd24cdd08348dc472c752612cd92f46753
procodrr <76770313+procodrr@users.noreply.github.com>
2022-12-03 17:27:15 +0530

    Add files via upload

---
+++ ep-29_functions-with-return-value/final-code/script.js
@@ -0,0 +1,9 @@
+function addTwoNumbers(a, b) {
+debugger
+
+    return a + b
+}
+
+// const result = addTwoNumbers(56, 6)
+
+// console.log(result);
---
+++ ep-29_functions-with-return-value/starter-code/script.js
@@ -0,0 +1,17 @@
+
+// Function Definition
+function introduceMe(username, profession, age) {
+    // console.log(typeof username);
+    // console.log(typeof profession);
+    // console.log(typeof age);
+    console.log('Hi,');
+    console.log(`My name is ${username || 'Procodrr'}.`);
+    console.log(`I am a ${profession}`);
+    console.log(`I am ${age} years old.`); 
+}
+
+// //                  Function Call
+// const returnValue = introduceMe()
+
+// introduceMe('Anurag', 'Software Engineer', 25)
+// introduceMe('Akash', 'Mechanical Engineer', 27)
