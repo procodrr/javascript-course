59de45e7ee3db1232f97d66842474d202bc39df9
procodrr <76770313+procodrr@users.noreply.github.com>
2022-12-04 20:57:08 +0530

    Add files via upload

---
+++ ep-30-execution-context-in-javascript/final-code/script.js
@@ -0,0 +1,23 @@
+debugger
+
+sayHi()
+
+const username = 'Anurag'
+const userAge = 25
+
+function sayHi() {
+    const a = 14
+    const b = 12
+    add(7, 9)
+}
+
+function add(x, y) {
+    kuchhBhi()
+    return x + y
+}
+
+function kuchhBhi() {
+    console.log('Kuchh bhi');
+}
+
+console.log('Program Ended');
---
+++ ep-30-execution-context-in-javascript/starter-code/script.js
@@ -0,0 +1,9 @@
+debugger
+console.log(firstName)
+
+var firstName = 'Akash'
+let lastName = 'Singh'
+let age = 15
+const yearOfBirth = 1999
+
+// let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
