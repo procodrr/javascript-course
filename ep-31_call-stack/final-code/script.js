ff094c1359e4faa12fd9b2132c6efaf8222c113c
procodrr <76770313+procodrr@users.noreply.github.com>
2022-12-06 07:46:11 +0530

    Add files via upload

---
+++ ep-31_call-stack/final-code/script.js
@@ -0,0 +1,30 @@
+debugger
+
+// sayHi()
+
+// const username = 'Anurag'
+// const userAge = 25
+
+// function sayHi() {
+//     const a = 14
+//     const b = 12
+//     add(7, 9)
+// }
+
+// function add(x, y) {
+//     kuchhBhi()
+//     return x + y
+// }
+
+// function kuchhBhi() {
+//     console.log('Kuchh bhi');
+// }
+
+function introduceMe() {
+    console.log('Hi, My name is Anurag.');
+    introduceMe()
+}
+
+introduceMe()
+
+console.log('Program Ended');
---
+++ ep-32-hoisting/final-code/script.js
@@ -0,0 +1,25 @@
+// debugger
+
+console.log(a)
+
+var a = 'Anurag'
+
+hi()
+
+
+// Function Definition
+// Function Declaration
+function hi() {
+    console.log('Hello');
+}
+
+// Function Definition
+// Function Expression
+var sayHi = function() {    //anonymous function
+    console.log('Hiii');
+}
+
+// IIFE
+
+
+sayHi()
