8559b715c6552aecf807fc8f2990a909abe0d2ae
procodrr <76770313+procodrr@users.noreply.github.com>
2022-12-07 23:05:30 +0530

    Add files via upload

---
+++ ep-33_scope-in-javascript/final-code/script.js
@@ -0,0 +1,24 @@
+const username = 'Anurag'
+let userAge = 25
+var a = 50
+
+function add() {
+  debugger
+  const username = 'Akash'
+  const x = 5
+  const y = 8
+  console.log(x + y)
+  console.log(username)
+}
+
+function subtract() {
+  const x = 15
+  const y = 18
+  console.log(x - y)
+  console.log(username)
+}
+
+add()
+subtract()
+
+console.log('Program Ended')
