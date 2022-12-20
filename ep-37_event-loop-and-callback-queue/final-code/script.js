efefaae3dd214a3acceacd2d86e46ecbe6e0e499
procodrr <76770313+procodrr@users.noreply.github.com>
2022-12-20 22:25:02 +0530

    Add files via upload

---
+++ ep-37_event-loop-and-callback-queue/final-code/script.js
@@ -0,0 +1,14 @@
+console.log('Hi-1');
+
+function hello() {
+    console.log('Hello World!');
+}
+
+for(let i = 1; i <=4; i++){
+    console.log(i);
+}
+
+hello()
+setTimeout(hello, 0)
+
+console.log('Hi-2');
