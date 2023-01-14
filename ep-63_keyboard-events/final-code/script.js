6c7ddcb507ae6f9eed7bdb923a56106af8f377bd
procodrr <76770313+procodrr@users.noreply.github.com>
2023-01-14 11:12:18 +0530

    Add files via upload

---
+++ ep-63_keyboard-events/final-code/script.js
@@ -0,0 +1,17 @@
+// const h1 = document.querySelector('h1')
+// const input = document.querySelector('input')
+
+// window.addEventListener('keypress', (e) => {
+//     console.log('Code: ',e.code);
+//     console.log('Value: ', e.key);
+// })
+
+// window.addEventListener('keyup', (e) => {
+//     console.log('Code: ',e.code);
+//     console.log('Value: ', e.key);
+// })
+
+window.addEventListener('keydown', (e) => {
+    console.log('Code: ',e.code);
+    console.log('Value: ', e.key);
+})
