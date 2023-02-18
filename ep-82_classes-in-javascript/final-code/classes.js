e4acb97735b73579c3ac91b8d313dcd264837c81
procodrr <76770313+procodrr@users.noreply.github.com>
2023-02-18 20:05:20 +0530

    Add files via upload

---
+++ ep-82_classes-in-javascript/final-code/classes.js
@@ -0,0 +1,17 @@
+class CreateUser {
+  constructor(firstName, lastName, age) {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+  }
+
+  getBirthYear() {
+    return new Date().getFullYear() - this.age
+  }
+  getFullName() {
+    return this.firstName + ' ' + this.lastName
+  }
+}
+
+const user1 = new CreateUser('Aman', 'Mishra', 32)
+const user2 = new CreateUser('Anurag', 'Singh', 72)
