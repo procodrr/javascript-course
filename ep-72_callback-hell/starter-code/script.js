132deac4d588a81dc8b1b6b0f83a3d158453c5a1
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2024-05-21 12:38:15 +0530

    Update script.js

--- ep-72_callback-hell/starter-code/script.js
+++ ep-72_callback-hell/starter-code/script.js
@@ -4,5 +4,5 @@ xhr.addEventListener('load', () => {
   console.log(xhr);
 })
 
-xhr.open('GET', 'https://6wrlmkp9u2.execute-api.us-east-1.amazonaws.com/?sleep=4000')
+xhr.open('GET', 'https://procodrr.vercel.app/?sleep=5000')
 xhr.send()
