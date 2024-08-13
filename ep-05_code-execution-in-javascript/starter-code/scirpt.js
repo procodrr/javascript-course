03fe7da72a1ce9656dd24d629998d4c75aa2a465
Anurag Singh Procodrr <76770313+procodrr@users.noreply.github.com>
2024-08-13 08:42:50 +0530

    Update scirpt.js

--- ep-05_code-execution-in-javascript/starter-code/scirpt.js
+++ ep-05_code-execution-in-javascript/starter-code/scirpt.js
@@ -1,16 +1,6 @@
-console.log(firstName)
-
-var firstName = 'Adarsh'
+var firstName = 'Akash'
 let lastName = 'Singh'
 let age = 15
-let खुशहै = true
-firstName = 100
-var a
+const yearOfBirth = 1999
 
-const hoursInDay = 24
-
-
-let userIntro = 
-    'Hi, my name is ' 
-    + firstName + ' ' 
-    + lastName + '. I am ' + age + ' ' + 'years old.'
+let userIntro = 'Hi, my name is ' + firstName + ' ' + lastName
